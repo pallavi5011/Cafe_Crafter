f23f04620b44480383bc1cd33fc5a8213ae0b69d
pallavi5011 <pallawi758@gmail.com>
2024-02-08 01:56:33 +0530

    Token-Based Functionality

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -72,6 +72,35 @@ namespace Cafe_Crafter.Controllers
             }
         }
 
+        [HttpGet, Route("checkToken")]
+        [CustomAuthenticationFilter]
+
+        public HttpResponseMessage checkToken()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { message = "true" });
+        }
+
+        [HttpGet, Route("getAllUser")]
+        [CustomAuthenticationFilter]
+
+        public HttpResponseMessage GetAllUser()
+        {
+            try {
+                var token = Request.Headers.GetValues("authorization").First();
+                TokenClaim tokenClaim = TokenManager.ValidateToken(token);
+                if(tokenClaim.Role != "admin")
+                {
+                    return Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+                var result = db.Users.Select(u => new { u.id, u.name, u.contactNumber, u.email, u.status, u.role, }).Where(x => (x.role == "user")).ToList();
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+                }
+
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
 
     }
 }
