b7c6951d2bb8fff41860ef95fc388d8b9ebbfd19
pallavi5011 <pallawi758@gmail.com>
2024-02-11 01:17:05 +0530

    Dash-Board

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using Cafe_Crafter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cafe_Crafter.Controllers
+{
+    [RoutePrefix("api/dashboard")]
+    public class DashboardController : ApiController
+    {
+        CafeEntities db = new CafeEntities();
+
+        [HttpGet,Route("details")]
+        [CustomAuthenticationFilter]
+
+        public HttpResponseMessage GetDetails()
+        {
+            try
+            {
+                var data = new
+                {
+                    category = db.Categories.Count(),
+                    product = db.Products.Count(),
+                    user = db.Users.Count()
+                };
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+
+            }
+        }
+    }
+}
